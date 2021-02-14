04b6153747c14c86a1a593f634c443278e666aed
draow <mhd.draw@gmail.com>
2021-02-14 19:29:08 +0300

    initial commit

---
+++ src/LogProxy.Core/DTO/MessageDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogProxy.Core.DTO
+{
+    public class MessageDTO
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Text { get; set; }
+
+        public DateTime ReceivedAt { get; set; }
+    }
+}
---
+++ src/LogProxy.Core/Interfaces/IMessageService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogProxy.Core.Interfaces
+{
+    public interface IMessageService
+    {
+
+    }
+}
